87351acf601e8cfbf64ea1457e5bbf105e5b4b01
Jordan Coakley <jordan_c1232@outlook.com>
2021-01-28 02:41:01 +0200

    Updates

---
+++ precache-manifest.8d635e7aff9b163cb971976acbbb8811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70f6193ac0b7b6509ca8c72c40969ff",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "df4af1d2607a8c266075",
+    "url": "/recipe-app/static/css/main.b8ffd932.chunk.css"
+  },
+  {
+    "revision": "f343ab71db138b443dd8",
+    "url": "/recipe-app/static/js/2.cee9095f.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/recipe-app/static/js/2.cee9095f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4af1d2607a8c266075",
+    "url": "/recipe-app/static/js/main.8a8b88d7.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
